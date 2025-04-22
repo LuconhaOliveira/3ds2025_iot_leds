44f0f9a0bcc1c883da2f2039fecb4bdfa8854407
LuconhaOliveira <lucaas.oliveira0254@gmail.com>
2025-04-22 10:21:13 -0300

    change to cloud database

--- data/conexao.py
+++ data/conexao.py
@@ -2,12 +2,22 @@ import mysql.connector
 
 class Conexao:
 
+    # def criar_conexao():
+    #     conexao = mysql.connector.connect(
+    #     host="localhost", 
+    #     port=3306, 
+    #     user="root", 
+    #     password="root", 
+    #     database="db_request"
+    #     )
+    #     return conexao
+
     def criar_conexao():
         conexao = mysql.connector.connect(
-        host="localhost", 
-        port=3306, 
-        user="root", 
-        password="root", 
-        database="db_request"
+        host="ds3-2025-iot-leds-aluno-d374.b.aivencloud.com", 
+        port=28179, 
+        user="avnadmin", 
+        password="AVNS_UQtCVbBpK2Bud9cV22F", 
+        database="defaultdb"
         )
         return conexao
