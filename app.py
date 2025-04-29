9e48f8e647d20b7c1f28837dc1e36731cd84dba0
LuconhaOliveira <lucaas.oliveira0254@gmail.com>
2025-04-29 10:50:22 -0300

    FIX: typing mistakes

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def desligar_led():
 def request():
     return jsonify(Request.recuperar_request())
 
-@app.route("    ")
+@app.route("/luzSala/ligar")
 def ligar_luz_sala():
     global estado_luz
     estado_luz = "ligadas"
